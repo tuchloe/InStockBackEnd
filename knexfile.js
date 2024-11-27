da3c62c737b699c8a779a2beeb1a0768a18c0146
Fosta Sampang <fosta.sampang@gmail.com>
2024-11-26 19:26:53 -0500

    update InStockBackEnd with files used from Instock Assets/DB Setup

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,15 @@
-// Import dotenv to process environment variables from `.env` file.
 import "dotenv/config";
+// Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 export default {
-  client: "mysql2",
+  client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    charset: "utf8",
-  },
+  }
 };

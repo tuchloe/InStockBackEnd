6d0238160985fd99d829a37c170ff7835b5fb0e0
i-am-lp <lp.phillips95@gmail.com>
2024-12-02 18:30:26 -0500

    created backend warehouse router

---
+++ routes/warehouse-router.js
@@ -0,0 +1,20 @@
+import express from "express";
+import knex from "knex";
+import knexConfig from "../knexfile.js";
+
+const router = express.Router();
+const db = knex(knexConfig.development); 
+
+router.get("/", async (_req, res) => {
+    try {
+      const warehouses = await db("warehouses").select("*");
+      res.json(warehouses);
+    } catch (error) {
+      console.error("Error fetching warehouses:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+
+});
+
+
+export default router;

b279b6d91886b3be7e0d699d1b01cc42ac9f0d0e
i-am-lp <lp.phillips95@gmail.com>
2024-12-08 10:42:12 -0500

    get individual warehouse inventory functionality added

--- routes/warehouse-router.js
+++ routes/warehouse-router.js
@@ -5,7 +5,7 @@ import knexConfig from "../knexfile.js";
 const router = express.Router();
 const db = knex(knexConfig);
 
-router.get("/", async (_req, res) => {
+router.get("/", async ( req, res) => {
     try {
       const warehouses = await db("warehouses").select("*");
       res.json(warehouses);
@@ -19,6 +19,7 @@ router.get("/", async (_req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
+    console.log(id);
     const warehouse = await db("warehouses").where({ id }).first();
     res.json(warehouse);
   } catch (error) {
@@ -27,9 +28,10 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/:id/inventories", async (_req, res) => {
+router.get("/:id/inventories", async ( req, res) => {
     try {
-      const { id } = _req.params;
+      const { id } =  req.params;
+      console.log(id);
       const warehouseInventory = await db("inventories").where({ warehouse_id: id });
       res.status(200).json(warehouseInventory);
     } catch (error) {
